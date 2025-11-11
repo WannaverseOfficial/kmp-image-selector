fb06ea39f1c10dd2b26943378bcc2cd54d3c73e3
Suraj Kumar <sk96.uk@gmail.com>
2025-11-11 20:01:40 +0000

    feat: add ImageBitmap.toByteArray function

--- imageselector/src/androidMain/kotlin/com/wannaverse/imageselector/ByteArrayExtension.kt
+++ imageselector/src/androidMain/kotlin/com/wannaverse/imageselector/ByteArrayExtension.kt
@@ -1,9 +1,19 @@
 package com.wannaverse.imageselector
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import java.io.ByteArrayOutputStream
 
 actual fun ByteArray.toImageBitmap(): ImageBitmap {
     return BitmapFactory.decodeByteArray(this, 0, size).asImageBitmap()
+}
+
+actual fun ImageBitmap.toByteArray(): ByteArray {
+    val bitmap = this.asAndroidBitmap()
+    val stream = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+    return stream.toByteArray()
 }
--- imageselector/src/jvmMain/kotlin/com/wannaverse/imageselector/ByteArrayExtension.kt
+++ imageselector/src/jvmMain/kotlin/com/wannaverse/imageselector/ByteArrayExtension.kt
@@ -1,13 +1,22 @@
 package com.wannaverse.imageselector
 
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toAwtImage
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import java.awt.image.BufferedImage
 import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
 
 actual fun ByteArray.toImageBitmap(): ImageBitmap {
     val inputStream = ByteArrayInputStream(this)
     val bufferedImage: BufferedImage = ImageIO.read(inputStream)
     return bufferedImage.toComposeImageBitmap()
+}
+
+actual fun ImageBitmap.toByteArray(): ByteArray {
+    val bufferedImage = this.toAwtImage()
+    val outputStream = ByteArrayOutputStream()
+    ImageIO.write(bufferedImage, "png", outputStream)
+    return outputStream.toByteArray()
 }
