31f5a924a70490f6849ae179c0164a312c09a82c
AdiK5050 <kumraditya456@gmail.com>
2026-06-14 14:19:25 +0530

    chore: updated docs for `selectImage` function.

--- imageselector/src/commonMain/kotlin/com/wannaverse/imageselector/ImageSelector.kt
+++ imageselector/src/commonMain/kotlin/com/wannaverse/imageselector/ImageSelector.kt
@@ -5,5 +5,10 @@ import androidx.compose.ui.unit.IntSize
 /**
  * Opens the platform's image picker and returns the selected [ImageData], or `null`
  * if the user cancels the selection.
+ * The image isn't loaded in the memory in its actual size, it's downsampled according to the
+ * current screen size and then loaded into memory as [ImageData] which is lower than the
+ * actual size eliminating the possibility of `OutOfMemoryError`
+ * @param reqResolution the screen size in which the image is needed to be shown.
+ * @param loadingState a callback to pass true if image is processing and false if it's not processing.
  */
 expect suspend fun selectImage(reqResolution: IntSize, loadingState: (Boolean) -> Unit): ImageData?
